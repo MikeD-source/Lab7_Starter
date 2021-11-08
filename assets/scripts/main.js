ccb7d10dc9813652ab128538cc62e3dd665846bd
MikeD-source <chenmiked17@gmail.com>
2021-11-07 23:11:59 -0800

    finish expose

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -25,6 +25,9 @@ const router = new Router(function () {
    * This will only be two single lines
    * If you did this right, you should see just 1 recipe card rendered to the screen
    */
+   document.querySelector('section.section--recipe-cards').classList.add('shown');
+   document.querySelector('section.section--recipe-expand').classList.remove('shown');
+    
 });
 
 window.addEventListener('DOMContentLoaded', init);
@@ -119,6 +122,22 @@ function createRecipeCards() {
    * After this step you should see multiple cards rendered like the end of the last
    * lab
    */
+    for (let i = 1; i < recipes.length; ++i) {
+      const recipeCard = document.createElement('recipe-card');
+      recipeCard.data = recipeData[recipes[i]];
+      if (i > 2) {
+        recipeCard.classList.add('hidden');
+      }
+      const page = recipeData[recipes[i]]['page-name'];
+      router.addPage(page, function() {
+        document.querySelector('.section--recipe-cards').classList.remove('shown');
+        document.querySelector('.section--recipe-expand').classList.add('shown');
+        document.querySelector('recipe-expand').data = recipeData[recipes[i]];
+      });
+      bindRecipeCard(recipeCard, page);
+
+      document.querySelector('.recipe-cards--wrapper').appendChild(recipeCard);
+    }
 }
 
 /**
@@ -174,6 +193,11 @@ function bindEscKey() {
    * if the escape key is pressed, use your router to navigate() to the 'home'
    * page. This will let us go back to the home page from the detailed page.
    */
+  document.addEventListener('keydown', e => {
+    if (e.code == 'Escape') {
+      router.navigate('home');
+    }
+  });
 }
 
 /**
@@ -195,4 +219,12 @@ function bindPopstate() {
    * so your navigate() function does not add your going back action to the history,
    * creating an infinite loop
    */
+  window.addEventListener('popstate', e => {
+    console.log(typeof e.state);
+    if (e.state) {
+      router.navigate(e.state, true);
+    } else {
+      router.navigate('home', true);
+    }
+  });
 }
